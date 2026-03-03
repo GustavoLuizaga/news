0d270c86fa8b6ab8e3c59778356e28b33f65d9f5
gustavo <luizagamerinogustavo1@gmail.com>
2026-03-03 14:49:22 -0400

    merge para deploy

--- news-backend/src/config/env.config.js
+++ news-backend/src/config/env.config.js
@@ -4,7 +4,8 @@ dotenv.config();
 const ENV = {
     ADMIN_USERNAME: process.env.ADMIN_USERNAME || 'admin',
     ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || 'admin123',
-    FILEPATH_NEWS: process.env.FILEPATH_NEWS || "./src/modules/news/content"
+    FILEPATH_NEWS: process.env.FILEPATH_NEWS || "./src/modules/news/content",
+    PORT: process.env.PORT || 3000,
 };
 
 export default ENV;
--- news-backend/src/index.js
+++ news-backend/src/index.js
@@ -1,9 +1,12 @@
-import Server from "./config/server.config.js";
+import ENV from "./config/env.config.js";
+import Server from "./config/server.config.js"
+import ENV from "./config/env.config.js";
+
 
 (() => {
     try {
-        Server.listen(3000, () => {
-            console.log("Server is running on port 3000");
+        Server.listen(ENV.PORT, () => {
+            console.log(`Server is running on port ${ENV.PORT}`);
         });
     } catch (error) {
         console.error("Error starting server:", error);
